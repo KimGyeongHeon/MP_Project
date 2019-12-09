f34430db8ab6205b5a4bdf550ce90f4b3b7469c2
KimGyeongHeon <45284822+KimGyeongHeon@users.noreply.github.com>
2019-12-09 13:36:11 +0900

    ㅇㅇ
    ㅇㅇ

--- app/src/main/java/com/example/mp_project/cafe99.java
+++ app/src/main/java/com/example/mp_project/cafe99.java
@@ -3,6 +3,7 @@ package com.example.mp_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +17,19 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class cafe99 extends AppCompatActivity implements OnMapReadyCallback {
+    Button dbb;
     Button cafeb;
     GoogleMap cafeMap;
+    SQLiteDatabase sqlDB;
+    cafe99Info.myDBHelper myHelper;
+    MarkerOptions Dom_Options = new MarkerOptions();
+    MarkerOptions N14_Options = new MarkerOptions();       MarkerOptions N7_Options=new MarkerOptions();
+    MarkerOptions N1_Options = new MarkerOptions();
+
+    MarkerOptions W8_Options = new MarkerOptions();
+
+    MarkerOptions E9_Options = new MarkerOptions();         MarkerOptions E1_1_Options = new MarkerOptions();
+    MarkerOptions E5_Options=new MarkerOptions();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,22 @@ public class cafe99 extends AppCompatActivity implements OnMapReadyCallback {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+        dbb=(Button)findViewById(R.id.cafe99B1);
+        dbb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sqlDB = myHelper.getWritableDatabase();
+                sqlDB.execSQL("insert into cafe99 values('"+N1_Options.getTitle().toString()+"',"+N1_Options.getSnippet().toString()+"',"+N1_Options.getTitle().toString()+");");
+                sqlDB.execSQL("insert into cafe99 values('"+N14_Options.getTitle().toString()+"',"+N14_Options.getSnippet().toString()+"',"+N14_Options.getTitle().toString()+");");
+                sqlDB.execSQL("insert into cafe99 values('"+N7_Options.getTitle().toString()+"',"+N7_Options.getSnippet().toString()+"',"+N7_Options.getTitle().toString()+");");
+                sqlDB.execSQL("insert into cafe99 values('"+Dom_Options.getTitle().toString()+"',"+Dom_Options.getSnippet().toString()+"',"+Dom_Options.getTitle().toString()+");");
+                sqlDB.execSQL("insert into cafe99 values('"+E1_1_Options.getTitle().toString()+"',"+E1_1_Options.getSnippet().toString()+"',"+E1_1_Options.getTitle().toString()+");");
+                sqlDB.execSQL("insert into cafe99 values('"+E5_Options.getTitle().toString()+"',"+E5_Options.getSnippet().toString()+"',"+E5_Options.getTitle().toString()+");");
+                sqlDB.execSQL("insert into cafe99 values('"+E9_Options.getTitle().toString()+"',"+E9_Options.getSnippet().toString()+"',"+E9_Options.getTitle().toString()+");");
+                sqlDB.execSQL("insert into cafe99 values('"+W8_Options.getTitle().toString()+"',"+W8_Options.getSnippet().toString()+"',"+W8_Options.getTitle().toString()+");");
+
             }
         });
     }
@@ -51,14 +79,7 @@ public class cafe99 extends AppCompatActivity implements OnMapReadyCallback {
         LatLng N1 = new LatLng(36.370174, 127.346107); //도서관 99
         LatLng N7 = new LatLng(36.371503, 127.344729);// 3학 99
 
-        MarkerOptions Dom_Options = new MarkerOptions();
-        MarkerOptions N14_Options = new MarkerOptions();       MarkerOptions N7_Options=new MarkerOptions();
-        MarkerOptions N1_Options = new MarkerOptions();
 
-        MarkerOptions W8_Options = new MarkerOptions();
-
-        MarkerOptions E9_Options = new MarkerOptions();         MarkerOptions E1_1_Options = new MarkerOptions();
-        MarkerOptions E5_Options=new MarkerOptions();
 
         N14_Options.position(N14);         N14_Options.snippet("생활과학대학 99");        N14_Options.title("N14 CAFE 99");             cafeMap.addMarker(N14_Options);
         N1_Options.position(N1);         N1_Options.snippet("도서관 99");        N1_Options.title("N1 CAFE 99");             cafeMap.addMarker(N1_Options);
